23a8905f5d66f38c38a9e3d4c5214c6c17c7735f
musou1500 <musou1500@gmail.com>
2020-04-05 02:46:23 +0900

    solve abc161 E, F

--- abc161/e.cpp
+++ abc161/e.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, const char *argv[])
 {
+  int n, k, c;
+  string s;
+  cin >> n >> k >> c >> s;
+
+  vector<int> d1;
+  for (int i = 0; i < n && d1.size() < k; i++) {
+    if (s[i] == 'o' && (d1.empty() || i - d1.back() > c)) {
+      d1.push_back(i);
+    }
+  }
+
+  vector<int> d2;
+  for (int i = n - 1; i >= 0 && d2.size() < k; i--) {
+    if (s[i] == 'o' && (d2.empty() || d2.back() - i > c)) {
+      d2.push_back(i);
+    }
+  }
+
+  vector<int> ans;
+  for (int i = 0; i < k; i++) {
+    if (d1[i] == d2[k - i - 1]) {
+      ans.push_back(d1[i]);
+    }
+  }
+
+  for (int i = 0; i < ans.size(); i++) {
+    cout << ans[i] + 1;
+    cout << (i == ans.size() - 1 ? '\n' : ' ');
+  }
   
   return 0;
 }
--- abc161/f.cpp
+++ abc161/f.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
+using ll = long long;
+
+vector<ll> get_divisors(ll n) {
+  vector<ll> ans;
+  for (ll i = 1; i * i <= n; i++) {
+    if (n % i == 0) {
+      ans.push_back(i);
+      if (i * i != n) {
+        ans.push_back(n / i);
+      }
+    }
+  }
+
+  return ans;
+}
+
+ll divsub(ll n, ll k) {
+  ll mod = n % k;
+  return mod == 0 ? divsub(n / k, k) : mod;
+}
+
 int main(int argc, const char *argv[])
 {
-  
+  ll n;
+  cin >> n;
+
+  ll ans = get_divisors(n - 1).size() - 1;
+  auto divisors = get_divisors(n);
+  for (auto divisor : divisors) {
+    if (divisor != 1 && divsub(n, divisor) == 1) {
+      ans++;
+    }
+  }
+
+  cout << ans << "\n";
   return 0;
 }
