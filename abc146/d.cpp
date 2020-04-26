95cd73039c3fc8e0a8995259c8ad10dc42abfa21
musou1500 <musou1500@gmail.com>
2020-04-26 20:27:09 +0900

    solve abc146 d

--- abc146/d.cpp
+++ abc146/d.cpp
@@ -1,77 +1,60 @@
-#include <iostream>
-#include <array>
-#include <vector>
-#include <set>
 #include <algorithm>
+#include <iostream>
+#include <queue>
+#include <vector>
 
 using namespace std;
 
-#include <unordered_map>
-
-using std::unordered_map;
-
-class UnionFind {
-  unordered_map<int, int> graph_;
-  unordered_map<int, int> sizes_;
-public:
-  int Root(int key) {
-    if (graph_.count(key) == 0 || graph_[key] == key) {
-      return key;
-    } else {
-      return graph_[key] = Root(graph_[key]);
-    }
-  }
-
-  bool Same(int key1, int key2) {
-    return Root(key1) == Root(key2);
-  }
-  
-  void Unite(int key1, int key2) {
-    if (Same(key1, key2)) {
-      return;
-    }
-    
-    int root1 = Root(key1);
-    int root2 = Root(key2);
-    int size = Size(root1) + Size(root2);
-    graph_[root1] = root2;
-    sizes_[root2] = size;
-  }
-
-  int Size(int key) {
-    int root = Root(key);
-    return sizes_.count(root) > 0 ? sizes_[root] : 1;
-  }
-};
-
-
-int main(int argc, const char *argv[])
-{
+int main(int argc, const char *argv[]) {
   int n;
   cin >> n;
-  vector<set<int>> vertices(n);
+  vector<vector<pair<int, int>>> vertices(n);
   for (int i = 0; i < n - 1; i++) {
-    int src, dest;
-    cin >> src >> dest;
-    vertices[src].insert(dest);
-    vertices[dest].insert(src);
+    int a, b;
+    cin >> a >> b;
+    a--;
+    b--;
+    vertices[a].push_back({i, b});
+    vertices[b].push_back({i, a});
   }
-  
-  // 辺数の最大値を求める
-  decltype(vertices)::iterator max_vertix_it, it;
-  int max_vertix = 0, i;
-  for (it = vertices.begin(), i = 0; it != vertices.end(); it++, i++) {
-    if (max_vertix_it->size() < it->size()) {
-      max_vertix_it = it;
-      max_vertix = i;
+
+  vector<int> ans(n - 1, 0);
+  vector<bool> visited(n, false);
+
+  // ノード番号，親の色
+  queue<pair<int, int>> q;
+  q.push({0, 0});
+
+  int max_n = 0;
+  while (!q.empty()) {
+    auto idx = q.front().first, p_color = q.front().second;
+    q.pop();
+    if (visited[idx]) {
+      continue;
     }
+
+    visited[idx] = true;
+
+    // v.first: 辺のインデックス
+    // v.second: 辺の向かう先
+    int color = 1;
+    for (auto v : vertices[idx]) {
+      if (p_color == color) {
+        color++;
+      }
+
+      if (ans[v.first] == 0) {
+        q.push({v.second, color});
+        ans[v.first] = color;
+        color++;
+      }
+    }
+
+    max_n = max(max_n, color - 1);
   }
 
-  cout << max_vertix_it->size() << "\n";
-
-  // 色を決定する
-  // 色の数だけUnion-Find木を持っておく
-  vector<UnionFind> ufs(max_vertix_it->size());
-  
-  
+  cout << max_n << '\n';
+  for (auto c : ans) {
+    cout << c << '\n';
+  }
 }
