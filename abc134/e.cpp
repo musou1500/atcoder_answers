53713919af9f8e5204d3bccffde2e9693fe7467d
musou1500 <musou1500@gmail.com>
2020-05-04 05:53:15 +0900

    solve abc134 e

--- abc134/e.cpp
+++ abc134/e.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <set>
+#include <vector>
 
 using namespace std;
 
-int main(int argc, const char *argv[])
-{
-  
+using ll = long long;
+
+int main(int argc, const char *argv[]) {
+  int n;
+  cin >> n;
+
+  vector<ll> va(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> va[i];
+  }
+
+  multiset<ll, greater<ll>> g;
+  for (auto &a : va) {
+    auto it = g.upper_bound(a);
+    if (it != g.end()) {
+      g.erase(it);
+    }
+
+    g.insert(a);
+  }
+
+  cout << g.size() << '\n';
+
   return 0;
 }
