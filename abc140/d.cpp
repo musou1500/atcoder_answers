da4a3965e967cf92a81e6be0a16cebf1e8bdbf6d
musou1500 <musou1500@gmail.com>
2020-05-05 02:49:25 +0900

    solve abc140 D

--- abc140/d.cpp
+++ abc140/d.cpp
@@ -8,5 +8,15 @@ int main(int argc, const char *argv[]) {
   int n, k;
   string s;
   cin >> n >> k >> s;
+
+  int cnt = 0;
+  for (int i = 0; i < s.size(); ++i) {
+    if (s[i] == s[i + 1]) {
+      cnt++;
+    }
+  }
+
+  cnt += 2 * k;
+  cout << min(n - 1, cnt) << '\n';
   return 0;
 }
