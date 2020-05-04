17a75d7348a74780978d371000adcdea6a46dec5
musou1500 <musou1500@gmail.com>
2020-05-05 06:46:14 +0900

    solve abc121 C

--- abc121/c.cpp
+++ abc121/c.cpp
@@ -3,6 +3,24 @@
 using namespace std;
 
 int main(int argc, const char *argv[]) {
-  //
+  long long n, m;
+  cin >> n >> m;
+
+  vector<pair<long long, long long>> vab(n);
+  for (auto &p : vab) {
+    cin >> p.first >> p.second;
+  }
+
+  sort(vab.begin(), vab.end());
+  long long ans = 0, cur = 0;
+  while (m > 0) {
+    long long cnt = min(m, vab[cur].second);
+    ans += vab[cur].first * cnt;
+    m -= cnt;
+    cur++;
+  }
+
+  cout << ans << '\n';
   return 0;
 }
+
