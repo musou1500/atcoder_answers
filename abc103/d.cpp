5564c99628e85885acf95f79750abb5bc2c6541a
musou1500 <musou1500@gmail.com>
2020-05-28 00:31:19 +0900

    fix abc103 d

--- abc103/d.cpp
+++ abc103/d.cpp
@@ -7,38 +7,24 @@ int main(int argc, const char *argv[]) {
   cin >> n >> m;
 
   vector<pair<int, int>> vab(m);
-  for (int i = 0; i < m; ++i) {
-    int a, b;
-    cin >> a >> b;
-    if (a > b) {
-      swap(a, b);
-    }
-
-    vab[i].first = a;
-    vab[i].second = b;
+  for (auto &p : vab) {
+    cin >> p.first >> p.second;
   }
 
   sort(vab.begin(), vab.end());
-  int l = vab[0].first, r = vab[0].second;
 
-  int ans = 0;
+  int l = vab[0].first, r = vab[0].second, ans = 1;
   for (int i = 1; i < m; ++i) {
-    int tmp_l = l, tmp_r = r;
     l = max(l, vab[i].first);
     r = min(r, vab[i].second);
-    // cout << r - l << '\n';
+
     if (r - l < 1) {
       ans++;
       l = vab[i].first;
       r = vab[i].second;
     }
-
-    if (i == m - 1) {
-      ans++;
-    }
   }
 
-  // cout << l << ' ' << r << '\n';
-  cout << max(1, ans) << '\n';
+  cout << ans << '\n';
   return 0;
 }
