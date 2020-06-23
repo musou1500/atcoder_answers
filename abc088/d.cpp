bc1afcc6b283d71e6af49a6832248e93cd2ba388
musou1500 <musou1500@gmail.com>
2020-06-24 00:05:34 +0900

    refactor abc088 d

--- abc088/d.cpp
+++ abc088/d.cpp
@@ -3,54 +3,38 @@
 using namespace std;
 
 int main(int argc, const char *argv[]) {
-  int h, w;
+  int h, w, cnt = 0;
   cin >> h >> w;
 
   vector<vector<char>> s(h, vector<char>(w));
-  int cnt = 0;
-  for (int i = 0; i < h; ++i) {
-    for (int j = 0; j < w; ++j) {
-      cin >> s[i][j];
-      if (s[i][j] == '#') {
-        cnt++;
-      }
+  for (auto &r : s) {
+    for (auto &c : r) {
+      cin >> c;
+      cnt += c == '#';
     }
   }
 
-  // (i, j, cost)
   vector<vector<int>> costs(h, vector<int>(w, -1));
   queue<array<int, 3>> q;
   q.push({0, 0, 0});
   while (!q.empty()) {
-    int i = q.front()[0], j = q.front()[1], cost = q.front()[2];
+    auto [i, j, cost] = q.front();
     q.pop();
-    if (costs[i][j] != -1 || s[i][j] == '#') {
-      continue;
-    }
-
-    costs[i][j] = cost;
-
-    if (i != 0) {
+    if (i >= 0 && i < h && j >= 0 && j < w && costs[i][j] < 0 &&
+        s[i][j] != '#') {
+      costs[i][j] = cost;
       q.push({i - 1, j, cost + 1});
-    }
-
-    if (i != h - 1) {
       q.push({i + 1, j, cost + 1});
-    }
-
-    if (j != 0) {
       q.push({i, j - 1, cost + 1});
-    }
-
-    if (j != w - 1) {
       q.push({i, j + 1, cost + 1});
     }
   }
 
-  if (costs[h - 1][w - 1] == -1) {
+  int cost = costs[h - 1][w - 1];
+  if (cost < 0) {
     cout << -1 << '\n';
   } else {
-    cout << h * w - (costs[h - 1][w - 1] + 1) - cnt << '\n';
+    cout << h * w - (cost + 1) - cnt << '\n';
   }
 
   return 0;
