e735d0c460fef4880ddb2e6172031a8b08cce20d
musou1500 <musou1500@gmail.com>
2020-06-24 22:05:43 +0900

    solve abc069

---
+++ abc069/a.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+  int n, m;
+  cin >> n >> m;
+  cout << (n - 1) * (m - 1) << '\n';
+  return 0;
+}
---
+++ abc069/b.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+  string s;
+  cin >> s;
+  cout << s[0] << s.size() - 1 << s.back();
+  return 0;
+}
